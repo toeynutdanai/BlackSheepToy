3a72adcf66090029d86013cc867b9f3f397d8c0e
Toey <teayvvv5@gmail.com>
2016-02-28 10:39:59 +0700

    Intent SignUp Finish

--- app/src/main/java/kmutnb/ratchaphol/natthawut/natdanai/blacksheeptoy/MainActivity.java
+++ app/src/main/java/kmutnb/ratchaphol/natthawut/natdanai/blacksheeptoy/MainActivity.java
@@ -1,7 +1,9 @@
 package kmutnb.ratchaphol.natthawut.natdanai.blacksheeptoy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    } //Main Method
+
+    public void clickSignUp(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
-}
+
+} //Main class
