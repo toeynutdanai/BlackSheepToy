5bb1a04848b9428d1ad7a44e6bb6eff91799f442
Toey <teayvvv5@gmail.com>
2016-04-06 16:25:03 +0700

    Delete SQLite Finish

--- app/src/main/java/kmutnb/ratchaphol/natthawut/natdanai/blacksheeptoy/MainActivity.java
+++ app/src/main/java/kmutnb/ratchaphol/natthawut/natdanai/blacksheeptoy/MainActivity.java
@@ -1,6 +1,7 @@
 package kmutnb.ratchaphol.natthawut.natdanai.blacksheeptoy;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,8 +19,27 @@ public class MainActivity extends AppCompatActivity {
 
         myManage = new MyManage(this);
 
+        //Test Add Value
+        //testAdd();
+
+        //Delete All SQLite
+        deleteAllSQLite();
+
 
     } //Main Method
+
+    private void deleteAllSQLite() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.product_table, null, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+    }
+
+    private void testAdd() {
+        myManage.addUser("name", "sur", "idcard", "user", "pass", "email", "phone");
+        myManage.addProduct("name", "brand", "price", "stock", "used",
+                "detail", "image1", "image2", "image3", "image4", "image5");
+    }
 
     public void clickSignUp(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
--- app/src/main/java/kmutnb/ratchaphol/natthawut/natdanai/blacksheeptoy/MyManage.java
+++ app/src/main/java/kmutnb/ratchaphol/natthawut/natdanai/blacksheeptoy/MyManage.java
@@ -1,5 +1,6 @@
 package kmutnb.ratchaphol.natthawut.natdanai.blacksheeptoy;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,12 +13,80 @@ public class MyManage {
     private MyOpenHelper myOpenHelper;
     private SQLiteDatabase sqLiteDatabase;
 
+    public static final String user_table = "userTABLE";
+    public static final String column_id = "_id";
+    public static final String column_Name = "Name";
+    public static final String column_Surname = "Surname";
+    public static final String column_idCard = "idCard";
+    public static final String column_User = "User";
+    public static final String column_Password = "Password";
+    public static final String column_Email = "Email";
+    public static final String column_Phone = "Phone";
+
+
+    public static final String product_table = "productTABLE";
+    public static final String column_Brand = "Brand";
+    public static final String column_Price = "Price";
+    public static final String column_Stock = "Stock";
+    public static final String column_Used = "Used";
+    public static final String column_Detail = "Detail";
+    public static final String column_Image1 = "Image1";
+    public static final String column_Image2 = "Image2";
+    public static final String column_Image3 = "Image3";
+    public static final String column_Image4 = "Image4";
+    public static final String column_Image5 = "Image5";
+
     public MyManage(Context context) {
 
         myOpenHelper = new MyOpenHelper(context);
         sqLiteDatabase = myOpenHelper.getWritableDatabase();
 
 
-
     } //Constructor
+
+    public long addProduct(String strName,
+                           String strBrand,
+                           String strPrice,
+                           String strStock,
+                           String strUsed,
+                           String strDetail,
+                           String strImage1,
+                           String strImage2,
+                           String strImage3,
+                           String strImage4,
+                           String strImage5) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_Name, strName);
+        contentValues.put(column_Brand, strBrand);
+        contentValues.put(column_Price, strPrice);
+        contentValues.put(column_Stock, strStock);
+        contentValues.put(column_Used, strUsed);
+        contentValues.put(column_Detail, strDetail);
+        contentValues.put(column_Image1, strImage1);
+        contentValues.put(column_Image2, strImage2);
+        contentValues.put(column_Image3, strImage3);
+        contentValues.put(column_Image4, strImage4);
+        contentValues.put(column_Image5, strImage5);
+        return sqLiteDatabase.insert(product_table, null, contentValues);
+    }
+
+
+    public long addUser(String strName,
+                        String strSurname,
+                        String strIDcard,
+                        String strUser,
+                        String strPassword,
+                        String strEmail,
+                        String strPhone) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_Name, strName);
+        contentValues.put(column_Surname, strSurname);
+        contentValues.put(column_idCard, strIDcard);
+        contentValues.put(column_User, strUser);
+        contentValues.put(column_Password, strPassword);
+        contentValues.put(column_Email, strEmail);
+        contentValues.put(column_Phone, strPhone);
+        return sqLiteDatabase.insert(user_table, null, contentValues);
+    }
+
 } // Main Class
