1904516c58bf6506177a7060b86b22164a007ab1
Toey <teayvvv5@gmail.com>
2016-05-08 11:41:02 +0700

    8/5/59 Create Activity Finish

---
+++ app/src/main/java/kmutnb/ratchaphol/natthawut/natdanai/blacksheeptoy/ToyDetail.java
@@ -0,0 +1,13 @@
+package kmutnb.ratchaphol.natthawut.natdanai.blacksheeptoy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToyDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toy_detail);
+    }
+}
---
+++ app/src/main/java/kmutnb/ratchaphol/natthawut/natdanai/blacksheeptoy/ToyListView.java
@@ -0,0 +1,13 @@
+package kmutnb.ratchaphol.natthawut.natdanai.blacksheeptoy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToyListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toy_list_view);
+    }
+}
