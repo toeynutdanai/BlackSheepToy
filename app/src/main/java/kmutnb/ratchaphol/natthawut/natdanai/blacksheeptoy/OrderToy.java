58619601a251359a868d7be47871e196d95a8ec6
Toey <teayvvv5@gmail.com>
2016-05-22 09:44:50 +0700

    Bill: Find name surname Finish

--- app/src/main/java/kmutnb/ratchaphol/natthawut/natdanai/blacksheeptoy/OrderToy.java
+++ app/src/main/java/kmutnb/ratchaphol/natthawut/natdanai/blacksheeptoy/OrderToy.java
@@ -1,13 +1,67 @@
 package kmutnb.ratchaphol.natthawut.natdanai.blacksheeptoy;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class OrderToy extends AppCompatActivity {
+
+    //Explicit
+    private String idString, dateString, sentToString,
+            productString, priceString;
+    private TextView nameUserTextView, surnameTextView, dateTextView;
+
+    private String[] useStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_toy);
+
+        //Bind Widget
+        BindWidget();
+
+        //find NameSurname
+        findNameSurname();
+
+    } // Main Method
+
+    private void findNameSurname() {
+        idString = getIntent().getStringExtra("ID_User");
+        Log.d("22MayV1", "idString ==> " + idString);
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM userTABLE WHERE _id = " + "'" + idString + "'", null);
+        cursor.moveToFirst();
+
+        useStrings = new String[cursor.getColumnCount()];
+        for (int i = 0; i < cursor.getColumnCount(); i++) {
+            useStrings[i] = cursor.getString(i);
+        } //for
+
+        nameUserTextView.setText(useStrings[1]);
+        surnameTextView.setText(useStrings[2]);
+
+    } //findNameSurname
+
+    private void BindWidget() {
+        nameUserTextView = (TextView) findViewById(R.id.textView16);
+        surnameTextView = (TextView) findViewById(R.id.textView17);
+        dateTextView = (TextView) findViewById(R.id.textView15);
     }
-}
+
+    public void clickAddMore(View view) {
+        finish();
+    }
+
+    public void clickCheckBill(View view) {
+
+    }
+
+} // Main Calss
