de626af879fd20c107c0f5c069af8ae35bb1cfec
Toey <teayvvv5@gmail.com>
2016-05-22 11:34:21 +0700

    Bill: Total Finish

--- app/src/main/java/kmutnb/ratchaphol/natthawut/natdanai/blacksheeptoy/OrderToy.java
+++ app/src/main/java/kmutnb/ratchaphol/natthawut/natdanai/blacksheeptoy/OrderToy.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -17,9 +18,9 @@ import java.util.Date;
 public class OrderToy extends AppCompatActivity {
 
     //Explicit
-    private String idString, dateString, sentToString,
-            productString, priceString;
-    private TextView nameUserTextView, surnameTextView, dateTextView;
+    private String idString, dateString, recieveNoString;
+    private TextView nameUserTextView, surnameTextView, dateTextView,
+            recieveNoTextView, totalTextView;
 
     private String[] useStrings;
     private ListView listView;
@@ -38,16 +39,36 @@ public class OrderToy extends AppCompatActivity {
         //Find Date
         findDate();
 
+        //Find receiveNo
+        findRecieveNo();
+
+
         //Create Listview
         createListView();
 
     } // Main Method
 
+    private void findRecieveNo() {
+
+        String[] resultStrings = dateString.split("/");
+        recieveNoString = "Ref-" +
+                useStrings[4] + "-" +
+                resultStrings[0] + "-" +
+                resultStrings[1] + "-" +
+                resultStrings[2];
+
+        recieveNoTextView.setText(recieveNoString);
+
+
+    }
+
     private void createListView() {
 
-        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+        int intTotal = 0;
+
+        final SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
                 MODE_PRIVATE, null);
-        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM orderTABLE", null);
+        final Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM orderTABLE", null);
         cursor.moveToFirst();
 
         String[] productStrings = new String[cursor.getCount()];
@@ -58,14 +79,41 @@ public class OrderToy extends AppCompatActivity {
 
             productStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.colunm_Product));
             priceStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Price));
+            priceInts[i] = Integer.parseInt(priceStrings[i]);
+
+            intTotal = intTotal + priceInts[i];
 
             cursor.moveToNext();
 
         }//for
+        cursor.close();
+
+        totalTextView.setText(Integer.toString(intTotal));
+
 
         ReceiveAdapter receiveAdapter = new ReceiveAdapter(this,
                 productStrings, priceStrings);
         listView.setAdapter(receiveAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                final SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                        MODE_PRIVATE, null);
+                final Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM orderTABLE", null);
+                cursor.moveToFirst();
+
+                cursor.moveToPosition(i);
+                int intID = Integer.parseInt(cursor.getString(0));
+                Log.d("22MayV2", "intID ==>" + intID);
+
+                sqLiteDatabase.delete("orderTABLE", "_id = " + intID, null);
+                createListView();
+
+            } //onItem
+        });
+        cursor.close();
 
     }//create listview
 
@@ -104,6 +152,8 @@ public class OrderToy extends AppCompatActivity {
         surnameTextView = (TextView) findViewById(R.id.textView17);
         dateTextView = (TextView) findViewById(R.id.textView15);
         listView = (ListView) findViewById(R.id.listView2);
+        recieveNoTextView = (TextView) findViewById(R.id.textView23);
+        totalTextView = (TextView) findViewById(R.id.textView19);
     }
 
     public void clickAddMore(View view) {
