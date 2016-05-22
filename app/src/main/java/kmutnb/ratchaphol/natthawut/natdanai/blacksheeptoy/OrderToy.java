6f20d2987c16f1fa42ea7732875e17146e403ba5
Toey <teayvvv5@gmail.com>
2016-05-22 10:20:58 +0700

    Bill: listview finish

--- app/src/main/java/kmutnb/ratchaphol/natthawut/natdanai/blacksheeptoy/OrderToy.java
+++ app/src/main/java/kmutnb/ratchaphol/natthawut/natdanai/blacksheeptoy/OrderToy.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.text.DateFormat;
@@ -21,6 +22,7 @@ public class OrderToy extends AppCompatActivity {
     private TextView nameUserTextView, surnameTextView, dateTextView;
 
     private String[] useStrings;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +38,36 @@ public class OrderToy extends AppCompatActivity {
         //Find Date
         findDate();
 
+        //Create Listview
+        createListView();
+
     } // Main Method
+
+    private void createListView() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM orderTABLE", null);
+        cursor.moveToFirst();
+
+        String[] productStrings = new String[cursor.getCount()];
+        String[] priceStrings = new String[cursor.getCount()];
+        int[] priceInts = new int[cursor.getCount()];
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+
+            productStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.colunm_Product));
+            priceStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Price));
+
+            cursor.moveToNext();
+
+        }//for
+
+        ReceiveAdapter receiveAdapter = new ReceiveAdapter(this,
+                productStrings, priceStrings);
+        listView.setAdapter(receiveAdapter);
+
+    }//create listview
 
     private void findDate() {
 
@@ -72,6 +103,7 @@ public class OrderToy extends AppCompatActivity {
         nameUserTextView = (TextView) findViewById(R.id.textView16);
         surnameTextView = (TextView) findViewById(R.id.textView17);
         dateTextView = (TextView) findViewById(R.id.textView15);
+        listView = (ListView) findViewById(R.id.listView2);
     }
 
     public void clickAddMore(View view) {
